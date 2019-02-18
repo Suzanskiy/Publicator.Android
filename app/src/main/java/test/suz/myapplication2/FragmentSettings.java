59a9312051d705df2b6b09ce1a1ec76d51510b18
Cузанский Сергей Юрьевич <suzanskiys@gmail.com>
2019-02-18 23:26:12 +0200

    new design plate

---
+++ app/src/main/java/test/suz/myapplication2/FragmentSettings.java
@@ -0,0 +1,19 @@
+package test.suz.myapplication2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSettings extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_settings, container,false);
+        return v;
+
+    }
+}
